8fc61d9fc4fc1bf86bd1c2b31927602b11c28545
Alexander Kramarev <pochemuto@yandex-team.ru>
2021-02-07 14:05:44 +0300

    secrets

--- src/main/java/com/pochemuto/homealone/HomealoneApplication.java
+++ src/main/java/com/pochemuto/homealone/HomealoneApplication.java
@@ -4,10 +4,13 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableAdminServer
 @ConfigurationPropertiesScan
+@PropertySource(name = "secret", value = {"file:/run/secrets/secret-properties"}, ignoreResourceNotFound = true)
+@PropertySource(name = "secret", value = {"file:${SECRET_PROPERTIES}"}, ignoreResourceNotFound = true)
 public class HomealoneApplication {
 
     public static void main(String[] args) {
