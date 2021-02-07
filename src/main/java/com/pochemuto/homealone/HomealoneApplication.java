d4dd69e6f431f94624a1ea7ebf865990f73307b3
Alexander Kramarev <pochemuto@yandex-team.ru>
2021-02-08 02:20:07 +0300

    extract spring boot admin server

--- src/main/java/com/pochemuto/homealone/HomealoneApplication.java
+++ src/main/java/com/pochemuto/homealone/HomealoneApplication.java
@@ -1,13 +1,11 @@
 package com.pochemuto.homealone;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@EnableAdminServer
 @ConfigurationPropertiesScan
 @PropertySource(name = "secret", value = {"file:/run/secrets/secret-properties"}, ignoreResourceNotFound = true)
 @PropertySource(name = "secret", value = {"file:${SECRET_PROPERTIES}"}, ignoreResourceNotFound = true)
@@ -16,5 +14,4 @@ public class HomealoneApplication {
     public static void main(String[] args) {
         SpringApplication.run(HomealoneApplication.class, args);
     }
-
 }
