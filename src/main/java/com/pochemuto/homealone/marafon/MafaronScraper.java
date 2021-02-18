7660f1e12969fbd4a4c4e7788d911705e0d38b0b
Alexander Kramarev <pochemuto@yandex-team.ru>
2021-02-18 23:58:47 +0300

    selenium container

--- src/main/java/com/pochemuto/homealone/marafon/MafaronScraper.java
+++ src/main/java/com/pochemuto/homealone/marafon/MafaronScraper.java
@@ -2,48 +2,38 @@ package com.pochemuto.homealone.marafon;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
 
-import javax.imageio.ImageIO;
-
 import com.google.common.base.Preconditions;
 import com.pochemuto.homealone.marafon.page.LoginPage;
 import com.pochemuto.homealone.marafon.page.SelectProgramPage;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import ru.yandex.qatools.ashot.AShot;
-import ru.yandex.qatools.ashot.Screenshot;
-import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
-import ru.yandex.qatools.ashot.shooting.cutter.CutStrategy;
-import ru.yandex.qatools.ashot.shooting.cutter.FixedCutStrategy;
 
 @Component
 public class MafaronScraper {
 
     private static final File IMG_FILE = Path.of(System.getProperty("user.home"), "Desktop", "img " + LocalDateTime.now().toString() + ".png").toFile();
+    private static final String SELENIUM_URL = "http://localhost:4444/wd/hub";
     @Autowired
     private MarafonProperties properties;
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--force-device-scale-factor=3.0");
 
-        WebDriver driver = new RemoteWebDriver(new URL("http://monica:4444/wd/hub"), options);
+        WebDriver driver = new RemoteWebDriver(new URL(SELENIUM_URL), options);
         JavascriptExecutor js = (JavascriptExecutor) driver;
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.navigate().to("https://lk.lerchekmarafon.ru/marafon/food?week=1&number=2");
