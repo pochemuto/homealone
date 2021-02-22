29444218b5212a7a7497aec3985089f020517afe
Александр Крамарев <pochemuto@yandex-team.ru>
2021-02-22 11:45:42 +0300

    add telegram health (#3)

---
+++ src/main/java/com/pochemuto/homealone/bot/TelegramHealthIndicator.java
@@ -0,0 +1,27 @@
+package com.pochemuto.homealone.bot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.objects.User;
+
+@Slf4j
+@Component
+public class TelegramHealthIndicator extends AbstractHealthIndicator {
+
+    @Autowired
+    private TelegramBot bot;
+
+    @Override
+    protected void doHealthCheck(Health.Builder builder) throws Exception {
+        log.debug("Checking bot status");
+        User botUser = bot.getMe();
+        log.debug("Registered as {}", botUser.getUserName());
+
+        builder.up()
+                .withDetail("login", botUser.getUserName())
+                .withDetail("id", botUser.getId());
+    }
+}
