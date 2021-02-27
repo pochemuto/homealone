60c7b5768cec75148c87b96527a9a70b766ff34f
Alexander Kramarev <pochemuto@yandex-team.ru>
2021-02-27 23:23:35 +0300

    application name in metrics

--- src/main/java/com/pochemuto/homealone/spring/MetricConfig.java
+++ src/main/java/com/pochemuto/homealone/spring/MetricConfig.java
@@ -1,6 +1,10 @@
 package com.pochemuto.homealone.spring;
 
 import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -8,8 +12,16 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @Configuration
 @EnableAspectJAutoProxy
 public class MetricConfig {
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Bean
     public TimedAspect timedAspect() {
         return new TimedAspect();
+    }
+
+    @Bean
+    public MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config().commonTags("application", applicationContext.getId());
     }
 }
