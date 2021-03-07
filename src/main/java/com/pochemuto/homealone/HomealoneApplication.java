66aca3a4b0adac23d61c52a64e5a8673e8ca4fa5
OriekhovKM <kiradnepr@gmail.com>
2021-03-07 20:57:43 +0200

    secret poroperties was added

--- src/main/java/com/pochemuto/homealone/HomealoneApplication.java
+++ src/main/java/com/pochemuto/homealone/HomealoneApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@PropertySource(name = "secret", value = {"file:${SECRET_PROPERTIES:C:/Users/Кирилл/IdeaProjects/homealone/secret.properties}"})
+@PropertySource(name = "secret", value = {"file:${SECRET_PROPERTIES:/run/secrets/secret-properties}"})
 public class HomealoneApplication {
 
     public static void main(String[] args) {
