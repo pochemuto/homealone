a1bfaa1fbee85fe00b02c7941e7c27ff2d5ca17f
Alexander Kramarev <pochemuto@yandex-team.ru>
2021-05-18 18:29:29 +0300

    WIP

--- src/main/java/com/pochemuto/homealone/bot/BotConfiguration.java
+++ src/main/java/com/pochemuto/homealone/bot/BotConfiguration.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import org.telegram.telegrambots.updatesreceivers.DefaultWebhook;
 
 @Configuration
 public class BotConfiguration {
@@ -24,4 +28,14 @@ public class BotConfiguration {
     public WebHookBot webHookBot() {
         return new WebHookBot(botProperties.getWebhookUrl());
     }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "bot", name = "webhook-path")
+    public TelegramBotsApi telegramBotsApi() throws TelegramApiException {
+        var webhook = new DefaultWebhook();
+        webhook.setInternalUrl("http://localhost:8081");
+        return new TelegramBotsApi(DefaultBotSession.class, webhook);
+    }
+
 }
+
